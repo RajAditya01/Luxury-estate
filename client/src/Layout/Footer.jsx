55da7a6519cb1e9a0562a3006b15a850eb83799a
Aditya Raj <101439988+RajAditya01@users.noreply.github.com>
2024-10-25 18:03:37 +0530

    Update Footer.jsx

--- client/src/Layout/Footer.jsx
+++ client/src/Layout/Footer.jsx
@@ -3,7 +3,7 @@ export default function Footer() {
         <footer className='bg-green-900 shadow-md mt-4'>
             <div className='max-w-6xl mx-auto p-3 flex justify-center'>
                 <p className='text-sm text-slate-200 text-center'>
-                    &copy; {new Date().getFullYear()} Developed by Ayush Ranjan
+                    &copy; {new Date().getFullYear()} Developed by Aditya Raj
                 </p>
             </div>
         </footer>
